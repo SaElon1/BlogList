e44995043adbcfcb0a3ab13dba1485528b445530
saelon <saelon@utu.fi>
2024-04-06 12:10:47 +0300

    jest, testit

---
+++ utils/list_helper.js
@@ -0,0 +1,16 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return 0
+    }
+    return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
+
+module.exports ={
+    dummy, totalLikes
+}
+
