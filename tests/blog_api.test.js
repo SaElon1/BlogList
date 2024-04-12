7195ff0ddd9bed7769ee7431088e0def3a718115
saelon <saelon@utu.fi>
2024-04-12 16:44:59 +0300

    4.7 and 4.8 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -6,8 +6,9 @@ const helper = require('./test_helper')
 
 const Blog = require('../models/blog')
 const { json } = require('express')
-const blog = require('../models/blog')
 
+
+describe('When there is initially some blogs saved', () => {
 beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(helper.testBlogs)
@@ -17,7 +18,9 @@ test('all blogs are returned', async () => {
     const response = await api.get('/api/blogs')
     expect(response.body).toHaveLength(helper.testBlogs.length)
 })
+})
 
+describe('addition of a new blog', () => {
 test('a valid blog can be added', async () => {
     const newBlog = {
         title: "TDD harms architecture",
@@ -36,6 +39,26 @@ test('a valid blog can be added', async () => {
 
     const blogTitles = blogsAtEnd.map(blog => blog.title)
     expect(blogTitles).toContain('TDD harms architecture')
+})
+})
+
+describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () =>{
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+        
+
+        await api
+        .delete(`/api/blogs/${blogToDelete._id}`)
+        .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+
+        const blogsAtEndIds = blogsAtEnd.map(blog => blog._id)
+        expect(blogsAtEndIds).not.toContain(blogToDelete._id)
+    })
+
 })
 
 afterAll(async () => {
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -21,6 +21,7 @@ const testBlogs = [
     }
 ]
 
+
 const blogsInDb = async () => {
     const blogs = await Blog.find({})
     return blogs.map(blog => blog.toJSON())
